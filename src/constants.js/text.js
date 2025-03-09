7750a72d1c23336d84ef16595ac064b33874eada
Nandan-Yadav <nandanyadav.putti@gmail.com>
2025-03-09 23:48:26 +0530

    Add default and login navigation items to constants

---
+++ src/constants.js/text.js
@@ -0,0 +1,14 @@
+export const default_navy_items = [
+    "Services",
+    "About Us",
+    "Sign Up",
+    "Sign In",
+]
+
+export const login_navy_items = [
+    "Services",
+    "About Us",
+    "Contact Us",
+    "Profile",
+    "Logout",
+]
